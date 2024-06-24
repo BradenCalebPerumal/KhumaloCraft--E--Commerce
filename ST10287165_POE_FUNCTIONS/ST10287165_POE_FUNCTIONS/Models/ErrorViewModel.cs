fa53d47cb1811efa4e53cf074e8afb41bb8dea2f
CalebPerumal28 <130969983+CalebPerumal28@users.noreply.github.com>
2024-06-24 20:05:24 +0200

    update

--- ST10287165_POE_FUNCTIONS/ST10287165_POE_FUNCTIONS/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CLDV6211_ST10287165_POE_P1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
